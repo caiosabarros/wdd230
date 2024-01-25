96810c7a7138e9fcefb4d0a834f081e52f2b46f2
Caio Barros <caiohenriquesa1@gmail.com>
2024-01-24 22:20:09 -0300

    w03

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,11 @@ let paragraph = document.getElementById("lastModified");
 if (paragraph) {
     paragraph.textContent = `Last Modified: ${lastModified}`;
 }
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
