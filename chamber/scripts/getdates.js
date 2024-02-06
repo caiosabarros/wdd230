b1161838006805800a9697188671097284056520
Caio Barros <caiohenriquesa1@gmail.com>
2024-02-06 17:24:31 -0300

    begin add dark mode

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -12,3 +12,19 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+// dark mode below
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
