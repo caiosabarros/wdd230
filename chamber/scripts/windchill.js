ad24352978c20fe5aec9035d88cba84b8cac5cd8
Caio Barros <caiohenriquesa1@gmail.com>
2024-02-15 22:52:47 -0300

    chamber:responsive images, srcset

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,16 @@
+let windspeed = document.getElementById("windspeed").innerHTML;
+windspeed = Number(windspeed.substr(11));
+let temperature = document.getElementById("temperature").innerHTML;
+temperature = Number(temperature.substr(0, 2));
+// check to make sure they meet the specification limits (<=50°F and>3.0mph) allowed to officially calculate the wind chill, and
+
+if (windspeed < 3 || temperature > 50) {
+    const windchill = document.getElementById("windchill");
+    windchill.innerHTML = "Windchill: N/A";
+    console.log("rip");
+} else {
+    // 35.74+0.6216*T=35.75*v^0.16+0,4275*T*v^0.16
+    const windchill = document.getElementById("windchill");
+    const windchillValue = 35.74 + 0.6216 * temperature - 35.75 * windspeed ** 0.16 + 0.4275 * temperature * windspeed ** 0.16;
+    windchill.innerHTML = `Windchill: ${windchillValue.toFixed(2)}`;
+}
